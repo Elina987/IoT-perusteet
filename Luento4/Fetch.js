e0054ab577e6d305c93ea09fa7a41c3f9510b47d
Elina987 <elinakekola@gmail.com>
2025-10-07 23:54:00 +0300

    Create Fetch.js

---
+++ Luento4/Fetch.js
@@ -0,0 +1,21 @@
+const url =
+  "https://api.thingspeak.com/channels/3100744/feeds.json?api_key=MBU1IAGQ5DZ9BEPR&";
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    const feeds = data.feeds;
+
+    const temperatures = feeds.map((feed) => ({
+      time: feed.created_at,
+      temp: parseFloat(feed.field1),
+    }));
+
+    document.getElementById("output").textContent =
+      JSON.stringify(temperatures);
+  })
+
+  .catch((error) => {
+    console.error("Error fetching data", error);
+    document.getElementById("output").textContent = "Error loading data";
+  });
